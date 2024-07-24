d5e478fcfc7d179145bb1637706b9c683b43171b
shadowsnavi <kumori.k.kuro@gmail.com>
2024-07-24 14:52:48 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching (source, find){
+    return source.filter(
+        (possibleMatch) => possibleMatch.toLowerCase() === find.toLowerCase()
+      )
+}
+
+function fuzzyMatch(source, possiblyMatch){
+    return source.filter(
+        (possibleMatch) =>
+          possibleMatch.toLowerCase().indexOf(possiblyMatch.toLowerCase()) === 0
+      )
+}
+
+function matchName(source, foundName){
+    return source.filter((record) => record.name === foundName);
+}
